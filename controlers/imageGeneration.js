353b57886bc97087b4af046cf3980e8935fbbe13
vladiyudi <plastikwood@gmail.com>
2024-11-19 16:22:48 +0200

    endpoints update

--- controlers/imageGeneration.js
+++ controlers/imageGeneration.js
@@ -4,7 +4,7 @@ async function generateImage(req, res) {
     const prompt = req.body.prompt
     const size = req.body.size
     const model = req.body.model
-    const result = await imageGeneration(prompt, size, model)
+    const result = await imageGeneration(prompt, size, model="flux-pro/new")
    
     res.status(200).json({
         message: "success",
