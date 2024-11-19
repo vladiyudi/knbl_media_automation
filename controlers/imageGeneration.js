717ab0e58fd7b6c947ddffab34c60e4128cb98e6
vladiyudi <plastikwood@gmail.com>
2024-11-19 16:22:11 +0200

    endpoints update

--- controlers/imageGeneration.js
+++ controlers/imageGeneration.js
@@ -3,7 +3,8 @@ const {imageGeneration} = require('../models/falAigen')
 async function generateImage(req, res) {
     const prompt = req.body.prompt
     const size = req.body.size
-    const result = await imageGeneration(prompt, size)
+    const model = req.body.model
+    const result = await imageGeneration(prompt, size, model)
    
     res.status(200).json({
         message: "success",
